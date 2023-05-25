e5eaa8a25ca79c9edd9853c31edf8e8da28a03d2
Druzai <g9code@yandex.ru>
2023-05-25 22:21:43 +0300

    Changed websocket endpoints

--- src/main/java/ru/mirea/secureapp/config/WebSocketConfig.java
+++ src/main/java/ru/mirea/secureapp/config/WebSocketConfig.java
@@ -18,7 +18,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/message-ws").withSockJS();
+        registry.addEndpoint("/message-ws").setAllowedOriginPatterns("*").withSockJS();
     }
 
 }
